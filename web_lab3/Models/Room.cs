f4a2d5710780172f5f15bcf2baf709bc93a45189
Jacob Bergqvist <Jacob.bergqvist@gmail.com>
2017-03-28 17:46:26 +0200

    First commit

---
+++ web_lab3/Models/Room.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace web_lab3.Models
+{
+    class Room
+    {
+        public int Id { get; set; }
+        [Required]
+        public String title { get; set; }
+        [Required]
+        public String description { get; set; }
+        [Required]
+        public double longitude { get; set; }
+        [Required]
+        public double cubeSide { get; set; }
+        public double latitude { get; set; }
+
+
+    }
+}
